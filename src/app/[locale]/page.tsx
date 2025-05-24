38d9dd69e640ecf928f8a6f2982680be8d75fbdf
Brahim Boussada <brahimboussada@protonmail.com>
2025-05-24 23:53:17 +0300

    hi

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/navigation";
+import { useLocale, useTranslations } from "next-intl";
+import { SiInstagram } from "@icons-pack/react-simple-icons";
+import { SiTiktok } from "@icons-pack/react-simple-icons";
+import { SiX } from "@icons-pack/react-simple-icons";
+import { SiGithub } from "@icons-pack/react-simple-icons";
+
+import Image from "next/image";
 
 const navigation = [
   { name: "Product", href: "#" },
@@ -8,61 +15,139 @@ const navigation = [
   { name: "Company", href: "#" },
 ];
 
-const jobListings = [
-  { title: "Sales Representative", location: "Remote" },
-  { title: "Metal Graphics Engineer", location: "Remote / NYC" },
-  { title: "Social Media Manager + Artist in Residence", location: "Remote" },
-  { title: "AI Artist / Hacker", location: "Remote" },
-  { title: "Machine Learning Engineer", location: "New York" },
-];
-
 export default function Home() {
+  const locale = useLocale();
   const t = useTranslations();
+  const jobs = [
+    "salesRepresentative",
+    "metalGraphicsEngineer",
+    "socialMediaManager",
+    "aiArtist",
+    "machineLearningEngineer",
+  ];
+
+  const navigation = ["roles", "contact", "download"];
+
+  const social = [
+    { name: "X", href: "#", icon: SiX },
+    { name: "Instagram", href: "#", icon: SiInstagram },
+    { name: "TikTok", href: "#", icon: SiTiktok },
+    { name: "GitHub", href: "#", icon: SiGithub },
+  ];
+
   return (
-    <main className="flex flex-col items-center gap-36 text-center max-w-3xl px-16 mx-auto my-36">
-      <section>
-        <h1 className="text-3xl font-medium mb-8">{t("Hero.title")}</h1>
-        <p className="">
-          Wand is a tool designed to help everyone bring their ideas to life-and
-          in the process, make us all a bit more divine. We feel that in order
-          to truly represent and explore our ideas accurately with Al, we need
-          tools that give us greater personalization and hands- on, visual
-          control over the generative models we use. We're building a
-          collaborative, native tool with these principles in mind-so that we
-          can help artists and design teams explore and spark new ideas. We're
-          an NYC based team of creatives and engineers passionate about artistic
-          expression and generative Al. We recently raised a seed round led by
-          OSV (partnering with Jim O'Shaughnessy, the executive chairman of
-          Stability.ai) along with a number of other great firms, and we're
-          looking to expand the team!
-        </p>
-      </section>
-
-      {/* TABLE */}
-      <section className="w-full">
-        <h1 className="text-3xl font-medium mb-8">Currently hiring</h1>
-
-        <div className=" border-t ">
-          {jobListings.map((job) => (
-            <div key={job.title} className="flex justify-between py-4 border-b">
-              <span>{job.title}</span>
-              <span>{job.location}</span>
-            </div>
+    <div className="max-w-7xl mx-auto px-4">
+      <header className="fixed top-4 inset-x-4 mx-auto xl:w-7xl flex items-center justify-between py-4 px-8 rounded-xl border bg-white shadow-xs">
+        <Link href="#" className="font-bold text-2xl">
+          Loam
+        </Link>
+        <nav className="flex gap-6" aria-label="Primary navigation">
+          {navigation.map((item) => (
+            <Link
+              key={item}
+              href={"#" + item}
+              className="transition-all duration-300 hover:text-muted-foreground"
+            >
+              {t(`Navigation.${item}.title`)}
+            </Link>
           ))}
-        </div>
-      </section>
+        </nav>
+      </header>
 
-      <section>
-        <h1 className="text-3xl font-medium mb-8">
-          Don't see a role that fits you?
-        </h1>
-        <p className="">
-          If you're passionate about what we're building & think you have
-          something to contribute to Wand, but your skills don't exactly fit
-          into any of the above roles, we still want to hear from you! Tell us
-          how you can help at info@wand.tech
-        </p>
-      </section>
-    </main>
+      {/* MAIN */}
+      <main className="flex flex-col items-center gap-36 text-center max-w-2xl  mx-auto my-36">
+        <section>
+          <h1 className="text-3xl font-medium mb-8">{t("Hero.title")}</h1>
+          <p>{t("Hero.paragraph")}</p>
+        </section>
+
+        {/* Role */}
+        <section className="w-full" id="roles">
+          <h2 className="text-3xl font-medium mb-8">{t("Table.title")}</h2>
+
+          <div className=" border-t ">
+            {jobs.map((job) => (
+              <div key={job} className="flex justify-between py-4 border-b">
+                <span>{t(`Table.${job}.title`)}</span>
+                <span>{t(`Table.${job}.location`)}</span>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        {/* CONTACT */}
+
+        <section id="contact">
+          <h2 className="text-3xl font-medium mb-8">{t("Contact.title")}</h2>
+          <div>
+            <p>
+              {t("Contact.paragraph")}{" "}
+              <Link href={`mailto:${t("Contact.email")}`}>
+                {t("Contact.email")}
+              </Link>
+            </p>
+          </div>
+        </section>
+      </main>
+
+      {/* Download */}
+
+      <footer id="download">
+        <div className="relative w-full h-[500px] md:h-[720px] overflow-hidden text-white max-w-6xl mx-auto">
+          {/* Background Image */}
+          <Image
+            className="object-cover rounded-xl "
+            src="/bg-footer.png"
+            quality={99}
+            alt=""
+            fill
+            priority
+          />
+
+          {/* Centered Text and Button */}
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">
+              {t("Download.title")}
+            </h1>
+            <Button
+              size={"lg"}
+              className="bg-blue-600 shadow-xl  hover:bg-blue-500  text-white text-base  py-2 px-4 rounded cursor-pointer"
+            >
+              {t("Download.ctl")}
+            </Button>
+          </div>
+
+          {/* Footer */}
+          <div className="absolute bottom-4 left-4 right-4 flex justify-between items-end text-sm text-white">
+            {/* Left side: Logo */}
+            <div className="font-bold cursor-not-allowed">Loam</div>
+
+            {/* Right side: Social Icons */}
+            <div className="flex space-x-4">
+              {social.map((item) => (
+                <Link
+                  key={item.name}
+                  className=" hover:text-muted-foreground transition-all duration-300"
+                  href={item.href}
+                  target="_blank"
+                >
+                  <item.icon size={16} />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="text-center text-xs py-4">
+          ©{" "}
+          {new Date()
+            .getFullYear()
+            .toLocaleString(locale === "ar" ? "ar-LB" : "en", {
+              useGrouping: false,
+            })}{" "}
+          {t("Footer.copyright")}
+        </div>
+      </footer>
+    </div>
   );
 }
